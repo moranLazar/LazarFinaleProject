588cbc59f8ae33240ab8a185d00129fb0293a951
moranLazar <moranlazar1995@gmail.com>
2025-01-27 10:53:57 +0200

    trying to fix counting in impo-ex

--- Training.py
+++ Training.py
@@ -139,7 +139,7 @@ class Training(threading.Thread):
      for i in range(2):
         self.run_exercise('impossible_EX')
         if s.have_voice :
-           say(s.req_exercise_inter-1)
+           say(self.what_to_say(s.req_exercise_inter-1))
            print(s.req_exercise_inter-1)
         else:
            s.screen.switch_frame(self.what_to_say(s.req_exercise_inter-1))
@@ -194,7 +194,7 @@ class Training(threading.Thread):
         for _ in range(reps):
             self.run_exercise('impossible_EX')
             if s.have_voice:
-                say(s.req_exercise_inter - 1)
+                say(self.what_to_say(s.req_exercise_inter-1))
                 print(s.req_exercise_inter-1)
             else:
                 s.screen.switch_frame(self.what_to_say(s.req_exercise_inter - 1))
@@ -227,7 +227,7 @@ class Training(threading.Thread):
      for _ in range(6):  # Wait for 60 seconds, doing reps, and checking for a wave
         self.run_exercise('impossible_EX')
         if s.have_voice :
-           say (s.req_exercise_inter-1)
+           say(self.what_to_say(s.req_exercise_inter-1))
         else:
            s.screen.switch_frame(self.what_to_say(s.req_exercise_inter-1))
         s.waved=self.interaction_mal()
